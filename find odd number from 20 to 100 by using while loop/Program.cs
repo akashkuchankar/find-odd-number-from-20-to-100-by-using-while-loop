ab9eee160d266b4db5925d6fc352c88ef88a072d
akashkuchankar <kuchankarakash@gmail.com>
2022-10-05 01:55:42 -0700

    Add project files.

---
+++ find odd number from 20 to 100 by using while loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_odd_number_from_20_to_100_by_using_while_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;
+            while (i <= 100)
+            {
+                if(i%2 != 0)
+                    Console.WriteLine(i);
+                i++;
+            }
+                
+        }
+    }
+}
